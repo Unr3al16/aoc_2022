5822f4d26123d832bc9010b9146b2defb329ef5c
Oliver <o.boettcher89@gmx.de>
2022-12-11 05:24:35 +0100

    Reformat Day10 script

--- src/main/kotlin/Day10/Day10.kt
+++ src/main/kotlin/Day10/Day10.kt
@@ -6,54 +6,50 @@ import utils.Loader
 
 class Day10 {
 
-    fun readSignalInstruction(){
+    fun readSignalInstruction() {
         val instructions = Loader.readFileAsLinesUsingReadLines("src/main/resources/data_aoc_10.txt")
-        val runCycles:List<Int> = listOf(20,60,100,140,180,220)
+        val runCycles: List<Int> = listOf(20, 60, 100, 140, 180, 220)
         var signalStrengthSum = 0
         runCycles.forEach { cycleCount ->
-            val signalStrength = calculateSignalStrengthAtCycle(cycleCount, instructions)* cycleCount
+            val signalStrength = calculateSignalStrengthAtCycle(cycleCount, instructions) * cycleCount
             signalStrengthSum += signalStrength
         }
         println(signalStrengthSum)
         drawSprite(instructions)
     }
 
-    private fun calculateSignalStrengthAtCycle(cylces:Int, instrctions:List<String>): Int{
+    private fun calculateSignalStrengthAtCycle(cylces: Int, instrctions: List<String>): Int {
         assert(cylces >= instrctions.size)
         var currentCycle = 2
         var strength = 1
         instrctions.forEach { inst ->
-            if (currentCycle > cylces)
-            {
+            if (currentCycle > cylces) {
                 return strength
             }
-            if (inst.startsWith("addx")){
+            if (inst.startsWith("addx")) {
                 currentCycle += 1
-                if (currentCycle > cylces)
-                {
+                if (currentCycle > cylces) {
                     return strength
                 }
                 strength += inst.split(" ")[1].toInt()
                 currentCycle += 1
-            }
-            else if (inst.startsWith("noop"))
-            {
+            } else if (inst.startsWith("noop")) {
                 currentCycle += 1
             }
         }
         return -1
     }
 
-    private fun drawSprite(instructions:List<String>){
+    private fun drawSprite(instructions: List<String>) {
         var currentPos = 1
         var cursorPos = 0
         var cycleIndex = 0
         instructions.forEach { inst ->
-            if (inst.startsWith("addx")){
+            if (inst.startsWith("addx")) {
                 cycleIndex += 1
 
                 printSign(cursorPos, currentPos)
-                if (cycleIndex % 40 == 0){
+                if (cycleIndex % 40 == 0) {
                     println()
                     cursorPos = 0
                     cycleIndex += 1
@@ -68,27 +64,26 @@ class Day10 {
                 cursorPos += 1
                 currentPos += inst.split(" ")[1].toInt()
             }
-        if (inst.startsWith("noop")){
-            cycleIndex += 1
-            printSign(cursorPos, currentPos)
-            cursorPos += 1
-        }
-        if (cycleIndex % 40 == 0){
-            println()
-            cursorPos = 0
-        }
+            if (inst.startsWith("noop")) {
+                cycleIndex += 1
+                printSign(cursorPos, currentPos)
+                cursorPos += 1
+            }
+            if (cycleIndex % 40 == 0) {
+                println()
+                cursorPos = 0
+            }
         }
 
     }
 
-    private fun printSign(cursorPos:Int, currentPos:Int){
+    private fun printSign(cursorPos: Int, currentPos: Int) {
         if (cursorPos == currentPos - 1 ||
             cursorPos == currentPos ||
-            cursorPos == currentPos + 1)
-        {
+            cursorPos == currentPos + 1
+        ) {
             print("#")
-        }
-        else{
+        } else {
             print(".")
         }
     }
